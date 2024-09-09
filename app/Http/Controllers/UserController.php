99e93d392a7f90e47afe5143ea669760bb367dd4
jirchin <jiruttikan2804@gmail.com>
2024-09-09 22:47:25 +0700

    add logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Userdb;
 use App\Models\Book;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
     //
     function profile(){
-        $info = Userdb::all();
+        $info = Userdb::where('username', Session::get('user')->username)->get();
+        // dd(Session::get('user')->username);
         $book = Book::all();
         return view('user.profile', compact('info'));
     }
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,11 +4,6 @@
 
 @push('style')
     <link rel="stylesheet" href="/css/user/profile.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
 @endpush
 
 @section('containerClassName', 'UserContainer')
@@ -32,7 +27,7 @@
                     <p>Facebook:</p>
                     <p>รหัสผ่าน: ******* <button id="change-password">เปลี่ยนรหัสผ่าน</button></p>
                     <p>เพศ:
-                        @if ($item->gender == 'F')
+                        @if($item->gender == 'F')
                             หญิง
                         @elseif($item->gender == 'M')
                             ชาย
@@ -42,7 +37,7 @@
             @endforeach
             <div class="user-actions">
                 <button id="edit-info-button">แก้ไขข้อมูล</button>
-                <button id="logout">ออกจากระบบ</button>
+                <button id="logout" onclick="window.location.href='/signin'">ออกจากระบบ</button>
             </div>
         </div>
 
@@ -76,7 +71,6 @@
         @endforeach
     </div>    
     
-
     <script>
         document.getElementById('user-menu').addEventListener('change', function() {
             document.querySelectorAll('.content-section', ).forEach(function(section) {
