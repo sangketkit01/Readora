39b8874c8f44e55a77123a264d1f3ac7756b1d81
jirchin <jiruttikan2804@gmail.com>
2024-09-14 02:37:00 +0700

    nothing

--- public/js/user/profile.js
+++ public/js/user/profile.js
@@ -3,17 +3,12 @@ const userOptions = document.querySelector('.user-options');
 const profileInfo = document.getElementById('profile-info');
 const editUserInfo = document.querySelector('.edit-user-info');
 const editPassword = document.querySelector('.edit-password');
-
 const editInfoButton = document.getElementById('edit-info');
-const changePasswordButton = document.getElementById('change-password');
-const linkEditPassword = document.getElementById('link-edit-password');
-
-function display(hideElement, showElement) {
-    hideElement.style.display = 'none';
-    showElement.style.display = 'block';
-}
+const changePasswordButton = document.getElementById('change-password-btn');
+const linkEditPassword = document.getElementById('link-change-password');
 
 profileInfo.style.display = 'block';
+
 userMenu.addEventListener('mouseover', () => userMenu.style.cursor = 'pointer');
 
 userMenu.addEventListener('change', function() {
@@ -23,21 +18,34 @@ userMenu.addEventListener('change', function() {
 });
 
 editInfoButton.addEventListener('click', () => {
-    display(profileInfo, editUserInfo);
+    profileInfo.style.display = 'none';
+    editUserInfo.style.display = 'block';
 });
 
-// changePasswordButton.addEventListener('click', () => {
-//     display(userOptions, editPassword);
-//     profileInfo.style.display = 'none';
-// });
+//popup
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.querySelector('.popup');
+    const blurLayer = document.createElement('div');
+    blurLayer.classList.add('blur-layer');
+    document.body.appendChild(blurLayer);
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
 
-
-// linkEditPassword.addEventListener('click', () => {
-//     display(userOptions, editPassword);
-//     profileInfo.style.display = 'none';
-//     editUserInfo.style.display = 'none';
-// });
-
+    document.querySelectorAll('#link-change-password, #change-password-btn').forEach(element => {
+        element.addEventListener('click', function(event) {
+            event.preventDefault();
+            popup.classList.add('active');
+            blurLayer.style.display = 'block';
+            overlay.style.display = 'block';
+        });
+    });
+    document.querySelector('.popup .close-btn').addEventListener('click', function() {
+        popup.classList.remove('active');
+        blurLayer.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+});
 
 
 
