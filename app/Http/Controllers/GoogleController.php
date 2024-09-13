5603ef184639a141d1d7f993e89b9e2d581095a1
sangketkit01 <Thiraphat_120@hotmail.com>
2024-09-14 02:35:32 +0700

    Fix Login

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Userdb;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -24,7 +25,30 @@ class GoogleController extends Controller
                 Session::put("google_email",$google_user->getEmail());
                 Session::put("google_avatar",$google_user->getAvatar());
 
-                return redirect()->route('sign_up');
+                $data = [
+                    "username" => $google_user->getEmail(),
+                    "profile" => $google_user->getAvatar(),
+                    "name" => $google_user->getName(),
+                    "email" => $google_user->getEmail(),
+                    "created_at" => now()
+                ];
+
+                Userdb::insert($data);
+
+                $user = DB::table("userdbs")->where("email",$google_user->getEmail())->first();
+                Session::flush();
+                Session::put("user", $user);
+
+
+                $data = [
+                    "avatar" => $google_user->getAvatar(),
+                    "name" => $google_user->getName(),
+                    "username" => $google_user->getEmail()
+                ];
+
+                Session::put("data", $data);
+
+                return redirect()->route("index");
             }
             Session::flush();
             Session::put("user",$user);
