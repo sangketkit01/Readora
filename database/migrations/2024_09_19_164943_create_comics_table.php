725bef407a5d7e663089b5b694cee21e8caebfff
Pawekuls <paweenakulsiri47@gmail.com>
2024-09-20 00:30:16 +0700

    create table comics/comic_types/comic_chapters

---
+++ database/migrations/2024_09_19_164943_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->increments("comicID");
+            $table->string("username");
+            $table->unsignedInteger("comicTypeID");
+            $table->string("comic_name");
+            $table->string("comic_pic");
+            $table->string("comic_description");
+            $table->tinyInteger("comic_status");
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign("username")->references("username")->on("userdbs")->onDelete("cascade");
+            $table->foreign("comicTypeID")->references("comicTypeID")->on("comic_types");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ database/migrations/2024_09_19_171557_create_comic_chapters_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comic_chapters', function (Blueprint $table) {
+            $table->increments("comicchapterID");
+            $table->string("chapter_image");
+            $table->unsignedInteger("comicID");
+            $table->unsignedInteger("comicTypeID");
+            $table->string("chapter_name");
+            $table->string("chapter_content");
+            $table->string("writer_message");
+            $table->tinyInteger("allow_comment");
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign("comicID")->references("comicID")->on("comics")->onDelete("cascade");
+            $table->foreign("comicTypeID")->references("comicTypeID")->on("comic_types")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comic_chapters');
+    }
+};
