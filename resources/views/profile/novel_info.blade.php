cfdf9e28fee564d0d25f837a0ac15f85601ace38
jirchin <jiruttikan2804@gmail.com>
2024-09-25 02:47:03 +0700

    style profile

--- resources/views/profile/novel_info.blade.php
+++ resources/views/profile/novel_info.blade.php
@@ -27,27 +27,67 @@
         </div>
     </div>
 
-    <div class="row mt-3">
-        <div class="dropdown-option">
-            <a class="btn btn-black dropdown-toggle fs-4 fw-semibold" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                นิยาย
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="{{ route('profile') }}">ข้อมูลส่วนตัว</a></li>
-                <li><a class="dropdown-item" href="{{ route('profile.comic') }}">คอมมิค</a></li>
-            </ul>
+    @if ($n_count == 0)
+        <div class="row mt-3">
+            <div class="dropdown-option">
+                <a class="btn btn-black dropdown-toggle fs-4 fw-semibold" href="#" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    นิยาย
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route('profile') }}">ข้อมูลส่วนตัว</a></li>
+                    <li><a class="dropdown-item" href="{{ route('profile.comic') }}">คอมมิค</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-    </div>
 
-    <div class="row mt-3">
-        @if ($n_count == 0)
-            <h3>มาสร้างผลงานเรื่องแรกกันเถอะ</h3>
-            กดสร้างเรื่องใหม่เพื่อลงผลงานของคุณ
-            <br><button class="create-novel">สร้างเรื่องใหม่</button>
-        @else
-        @endif
-    </div>
+        <div class="row mt-5">
+            <div class="new">
+                <h3>มาสร้างนิยายเรื่องแรกกันเถอะ</h3>
+                กดสร้างเรื่องใหม่เพื่อลงผลงานของคุณ <br>
+                <button class="create-novel" onclick="window.location.href='/'">สร้างเรื่องใหม่</button>
+            </div>
+        </div>
+    @else
+        <div class="row mt-3">
+            <div class="dropdown-option">
+                <a class="btn btn-black dropdown-toggle fs-4 fw-semibold" href="#" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    นิยาย
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route('profile') }}">ข้อมูลส่วนตัว</a></li>
+                    <li><a class="dropdown-item" href="{{ route('profile.comic') }}">คอมมิค</a></li>
+                </ul>
+                <button class="create-novel-n" onclick="window.location.href=''">สร้างเรื่องใหม่</button>
+            </div>
+
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-3">
+                @foreach ($novel as $n)
+                    <img src="{{$n->book_pic}}" alt="">
+                    {{$n->book_name}} <br>
+                    {{$n->username}} <br>
+                    <span></span> 
+                    {{$n->Genre->bookGenre_name}}
+                @endforeach
+            </div>
+
+        </div>
+    @endif
+
+
+    {{-- $table->increments("bookID");
+            $table->string("username");
+            $table->unsignedInteger("bookTypeID");
+            $table->unsignedInteger("bookGenreID");
+            $table->string("book_name");
+            $table->string("book_pic");
+            $table->text("book_description");
+            $table->string("book_status",7)->default("public");
+            $table->timestamps();
+            $table->softDeletes(); --}}
 
 @endsection
