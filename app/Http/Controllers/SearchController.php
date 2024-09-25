b9d0acbe415c3643ba2dbd4ac5ed8e74b81c9d14
Autthapon <autthapon.a@kku.ac.th>
2024-09-26 01:24:20 +0700

    serch done update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,20 +13,10 @@ class SearchController extends Controller
         $query = $request->input('query');
 
         // ค้นหาหนังสือจากฐานข้อมูล
-        // คุณสามารถปรับแต่งเงื่อนไขการค้นหาได้ตามต้องการ
         $books = Book::where('book_name', 'LIKE', "%{$query}%")
                     ->orWhere('book_description', 'LIKE', "%{$query}%")
-                    ->orWhereHas('User', function($q) use ($query) {
-                        $q->where('username', 'LIKE', "%{$query}%");
-                    })
-                    ->orWhereHas('Type', function($q) use ($query) {
-                        $q->where('bookType_name', 'LIKE', "%{$query}%");
-                    })
-                    ->orWhereHas('Genre', function($q) use ($query) {
-                        $q->where('bookGenre_name', 'LIKE', "%{$query}%");
-                    })
+                    ->where('username', 'LIKE', "%{$query}%")
                     ->get();
-
         // ส่งผลลัพธ์การค้นหาไปที่ view
         return view('user.search-result', compact('books', 'query'));
     }
